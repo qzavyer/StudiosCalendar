87169f6e63e949b55a12b046add9dcd0cb6a950b
qzavyer <qzavyer@mail.ru>
2016-12-29 10:16:02 +0500

    Получение адресов календарей из базы

--- app/src/main/java/com/andr/qzavyer/studioscalendar/DayActivity.java
+++ app/src/main/java/com/andr/qzavyer/studioscalendar/DayActivity.java
@@ -1,43 +1,41 @@
 package com.andr.qzavyer.studioscalendar;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DayActivity extends AppCompatActivity {
     public static final String[] EVENT_PROJECTION = new String[]{
             CalendarContract.Events._ID,                            // 0
-            CalendarContract.Events.ACCOUNT_NAME,                   // 1
-            CalendarContract.Events.CALENDAR_DISPLAY_NAME,          // 2
-            CalendarContract.Events.OWNER_ACCOUNT,                  // 3
-            CalendarContract.Events.STATUS,                         // 4
-            CalendarContract.Events.DTSTART,                        // 5
-            CalendarContract.Events.CALENDAR_ID,                    // 6
+            CalendarContract.Events.STATUS,                         // 1
+            CalendarContract.Events.DTSTART,                        // 2
+            CalendarContract.Events.DTEND                           // 3
     };
 
-    public static final String[] CAL_PROJ = new String[]{
+    public static final String[] CALENDAR_PROJECTION = new String[]{
             CalendarContract.Calendars._ID,
             CalendarContract.Calendars.NAME
     };
 
     // The indices for the projection array above.
     private static final int PROJECTION_ID_INDEX = 0;
-    private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
-    private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
-    private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
-    private static final int PROJECTION_STATUS = 4;
-    private static final int PROJECTION_DTSTART = 5;
-    private static final int PROJECTION_CALENDARID = 6;
+    private static final int PROJECTION_STATUS = 1;
+    private static final int PROJECTION_DTSTART = 2;
+    private static final int PROJECTION_DTEND = 3;
     private static final int REQUEST_LOCATION = 2;
 
     LinearLayout dayMain;
@@ -50,59 +48,73 @@ public class DayActivity extends AppCompatActivity {
         ContentResolver cr = getContentResolver();
         Uri uri = CalendarContract.Events.CONTENT_URI;
         Uri calUri = CalendarContract.Calendars.CONTENT_URI;
-        String calSel = "((" +
-                CalendarContract.Calendars.NAME + " = ?"+
-                "))";
-        String[] calArgs = new String[]{"<account>@gmail.com"};
-        String selection = "((" + CalendarContract.Events.CALENDAR_ID + " = ?) AND (" +
-                CalendarContract.Events.ACCOUNT_NAME + " = ?)"+
-                ")";
-        // Submit the query and get a Cursor object back.
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_CALENDAR},
-                    REQUEST_LOCATION);
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+        String calSel = "(" + CalendarContract.Calendars.NAME + " = ?)";
+        DBHelper dbHelper = new DBHelper(this);
+        @SuppressLint("UseSparseArrays")
+        Map<Integer, String> addresses = new HashMap<>();
+        // подключаемся к БД
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        // делаем запрос всех данных из таблицы studios, получаем Cursor
+        Cursor c = db.query("studios", null, null, null, null, null, null);
+
+        // ставим позицию курсора на первую строку выборки
+        if (c.moveToFirst()) {
+            // определяем номера столбцов по имени в выборке
+            int idColIndex = c.getColumnIndex("id");
+            int nameColIndex = c.getColumnIndex("calendar");
+            do {
+                // получаем значения по номерам столбцов и пишем все в лог
+                addresses.put(c.getInt(idColIndex), c.getString(nameColIndex));
+                // переход на следующую строку
+                // а если следующей нет (текущая - последняя), то выходим из цикла
+            } while (c.moveToNext());
         }
-        try{
-            Cursor calCur = cr.query(calUri, CAL_PROJ, calSel, calArgs, null);
-            long calID = 0;
-            while (calCur.moveToNext()) {
+        c.close();
 
-                String name;
-
-                // Get the field values
-                calID = calCur.getLong(PROJECTION_ID_INDEX);
-                name = calCur.getString(PROJECTION_ACCOUNT_NAME_INDEX);
-                long id = calID/2;
+        for(Map.Entry<Integer, String> entry : addresses.entrySet()) {
+            String[] calArgs = new String[]{entry.getValue()};
+            String selection = "((" + CalendarContract.Events.CALENDAR_ID + " = ?) AND (" +
+                    CalendarContract.Events.ACCOUNT_NAME + " = ?))";
+            // Проверка разрешения чтения календаря, если нет, запрашиваем разрешение
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_CALENDAR},
+                        REQUEST_LOCATION);
+                return;
             }
-            String[] selectionArgs = new String[]{Long.toString(calID),"<account>@gmail.com"};
-            Cursor cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
-            while (cur.moveToNext()) {
-                String displayName = null;
-                String accountName = null;
-                String ownerName = null;
-                String statusName = null;
-                String startName = null;
-
-                // Get the field values
-                accountName = cur.getString(PROJECTION_ACCOUNT_NAME_INDEX);
-                ownerName = cur.getString(PROJECTION_OWNER_ACCOUNT_INDEX);
-                statusName = cur.getString(PROJECTION_STATUS);
-                startName = cur.getString(PROJECTION_DTSTART);
-                displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX);
+            try {
+                // отправка запроса на получение календаря
+                @SuppressLint("Recycle")
+                Cursor calCur = cr.query(calUri, CALENDAR_PROJECTION, calSel, calArgs, null);
+                long calID = 0;
+                if (calCur != null) {
+                    while (calCur.moveToNext()) {
+                        // Получаем ИД календаря
+                        calID = calCur.getLong(PROJECTION_ID_INDEX);
+                    }
+                }
+                String[] selectionArgs = new String[]{Long.toString(calID), entry.getValue()};
+                @SuppressLint("Recycle")
+                Cursor cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
+                if (cur != null) {
+                    while (cur.moveToNext()) {
+                        long eidName = 0;
+                        String endName = null;
+                        String statusName = null;
+                        String startName = null;
+                        // Get the field values
+                        eidName = cur.getLong(PROJECTION_ID_INDEX);
+                        statusName = cur.getString(PROJECTION_STATUS);
+                        startName = cur.getString(PROJECTION_DTSTART);
+                        endName = cur.getString(PROJECTION_DTEND);
+                    }
+                }
+            } catch (Exception e) {
+                String msg = e.getMessage();
             }
-        }catch (Exception e){
-            String msg = e.getMessage();
         }
         dayMain = (LinearLayout)findViewById(R.id.dayMain);
+        Date today = new Date();
         String[] data = {
                 "00:00 - 01:00",
                 "01:00 - 02:00",
@@ -129,22 +141,10 @@ public class DayActivity extends AppCompatActivity {
                 "22:00 - 23:00",
                 "23:00 - 00:00"};
 
-        LinearLayout item = new LinearLayout(this);
         for(String point : data){
             TextView headView = new TextView(this);
             headView.setText(point);
             dayMain.addView(headView);
         }
-
-        /*ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.gridcell, R.id.tvText, data);
-        daysGrid.setAdapter(adapter);*/
-        adjustGridView();
-
-
-
-    }
-    private void adjustGridView() {
-        /*daysGrid.setNumColumns(2);
-        daysGrid.setColumnWidth(100);*/
     }
 }
